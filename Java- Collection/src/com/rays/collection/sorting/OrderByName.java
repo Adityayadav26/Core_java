4a3063c1013852c09ca10365c069748bb51b1eee
Adityayadav26 <adityayadav7806@gmail.com>
2026-01-02 13:55:44 +0530

    ok

---
+++ Java- Collection/src/com/rays/collection/sorting/OrderByName.java
@@ -0,0 +1,14 @@
+package com.rays.collection.sorting;
+
+import java.util.Comparator;
+
+public class OrderByName implements Comparator<Employee>{
+
+	@Override
+	public int compare(Employee o1, Employee o2) {
+		return o1.name.compareTo(o2.name); //sort by name is asc
+	//	return o2.name.compareTo(o1.name); // sort by name is desc
+	}
+	
+
+}
